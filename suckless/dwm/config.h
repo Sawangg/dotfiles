8f35160a2d790efc464aa472706d565f0c07ca04
Sawangg <le1112osee@gmail.com>
2024-01-26 20:48:35 +0100

    fix(dwm): fix kitty spawn

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", "--config", "/home/$USER/.config/kitty/kitty.conf", NULL };
+static const char *termcmd[]  = { "kitty", NULL, "--config", "/home/$USER/.config/kitty/kitty.conf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
