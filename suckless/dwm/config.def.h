0c530bf44abcfb68f2f3d0ff9d2cbde6cef6c840
Sawangg <le1112osee@gmail.com>
2024-01-29 15:50:15 +0100

    feat(slstatus): added slstatus

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 7;        /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10:antialias=true:autohint=true", "Hack:size=24" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
