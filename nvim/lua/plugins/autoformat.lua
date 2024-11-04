1bf0476b0b7eda6a8f603ca132b4d2ba095cbf81
Sawangg <leo.mercier1@proton.me>
2024-11-05 00:03:01 +0100

    fix(nvim): using recommanded conform format to fix old syntax

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -1,28 +1,30 @@
 return {
   { -- Autoformat your document
     "stevearc/conform.nvim",
-    lazy = false,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
         "<leader>f",
         function()
-          require("conform").format({ async = true, lsp_fallback = true })
+          require("conform").format({ async = true })
         end,
         mode = "",
         desc = "[F]ormat buffer",
       },
     },
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "biome", "prettierd" } },
-        typescript = { { "biome", "prettierd" } },
+        javascript = { "biome", "prettierd", stop_after_first = true },
+        typescript = { "biome", "prettierd", stop_after_first = true },
       },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      format_on_save = { timeout_ms = 500 },
     },
   },
   { -- Set buffer options automatically
