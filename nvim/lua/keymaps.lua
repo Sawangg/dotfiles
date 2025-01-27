cefe692003961bb227fa5096eb2bdaa3303d411e
Sawang <leo.mercier1@proton.me>
2025-01-27 17:28:27 +0100

    fix(nvim): removed unused keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,8 +9,6 @@ end
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-map("n", "[d", vim.diagnostic.jump({ count = -1, float = true }), { desc = "Go to previous [D]iagnostic message" })
-map("n", "]d", vim.diagnostic.jump({ count = 1, float = true }), { desc = "Go to next [D]iagnostic message" })
 map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
