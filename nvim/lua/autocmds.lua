7da2e611b5fe7bace6e3c69d595581ce53af555e
Sawangg <leo.mercier1@proton.me>
2025-01-29 22:42:23 +0100

    feat(nvim): autocmd to open file explorer if opened with no file

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,12 +1,22 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Open file explorer when starting Vim without a file",
+  group = vim.api.nvim_create_augroup("OpenFolderView", { clear = true }),
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.defer_fn(function()
+        require("oil").open()
+      end, 1)
+    end
   end,
 })
