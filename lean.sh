40bc85448b557a69fd7296d8c2cd705b4190ba75
Sawang <leo.mercier1@proton.me>
2025-05-28 11:21:49 +0200

    feat(devcontainer): installing neovim from tar release

--- lean.sh
+++ lean.sh
@@ -26,18 +26,36 @@ fi
 if command -v apt-get > /dev/null 2>&1; then
     printf "%sUsing package manager: %sapt-get%s\n" "$CYAN" "$BOLD" "$RESET"
     $SUDO apt-get update
-    $SUDO apt-get install -y stow fzf neovim ripgrep starship zoxide
+    $SUDO apt-get install -y stow fzf ripgrep starship zoxide curl
 elif command -v apk > /dev/null 2>&1; then
     printf "%sUsing package manager: %sapk%s\n" "$CYAN" "$BOLD" "$RESET"
     $SUDO apk update
-    $SUDO apk add stow fzf neovim ripgrep starship zoxide
+    $SUDO apk add stow fzf ripgrep starship zoxide curl
 elif command -v pacman > /dev/null 2>&1; then
     printf "%sUsing package manager: %spacman%s\n" "$CYAN" "$BOLD" "$RESET"
-    $SUDO pacman -Sy --noconfirm stow fzf neovim ripgrep starship zoxide
+    $SUDO pacman -Sy --noconfirm stow fzf ripgrep starship zoxide curl
 else
     printf "%sError: No supported package manager found%s\n" "$RED" "$RESET"
     exit 1
 fi
+
+printf "%sInstalling latest Neovim from GitHub releases...%s\n" "$CYAN" "$RESET"
+
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR"
+
+NVIM_TARBALL="nvim-linux-$(uname -m | sed 's/aarch64/arm64/').tar.gz"
+printf "%sDownloading %s...%s\n" "$CYAN" "$NVIM_TARBALL" "$RESET"
+
+curl -LO "https://github.com/neovim/neovim/releases/latest/download/${NVIM_TARBALL}"
+
+$SUDO tar -C /opt -xzf "$NVIM_TARBALL"
+$SUDO ln -sf "/opt/nvim-linux-${NVIM_ARCH}/bin/nvim" /usr/local/bin/nvim
+
+cd - > /dev/null
+rm -rf "$TEMP_DIR"
+
+touch "$HOME/dotfiles/nvim/lua/custom.lua"
 
 stow -R .
 ln -sf "$HOME/dotfiles/.bashrc" ~/.bashrc
