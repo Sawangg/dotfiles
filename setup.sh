270c5a2135559b0de87cea30bebcb5468c5ec8e9
Sawang <leo.mercier1@proton.me>
2025-05-28 10:20:25 +0200

    fix(setup): added support for git clone even if conflicts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 readonly RESET="\033[0m"
 readonly RED="\033[0;31m"
 readonly GREEN="\033[0;32m"
@@ -27,12 +29,12 @@ fi
 
 if [ "$(id -u)" = "0" ]; then
   printf "${GREEN}Running as root. No privilege elevation needed.${RESET}\n"
-  ELEVATED_PRIVILEGE_CMD=""
+  SUDO=""
 else
   if check_command doas; then
-    ELEVATED_PRIVILEGE_CMD=doas
+    SUDO=doas
   elif check_command sudo; then
-    ELEVATED_PRIVILEGE_CMD=sudo
+    SUDO=sudo
   else
     printf "${RED}✗ Sudo or doas is not installed and is required for non-root users! Please install one of them manually!${RESET}\n"
     exit 1
@@ -57,7 +59,7 @@ if ! check_command git; then
   printf "${YELLOW}Git is not installed and is required, do you wish to install it? (y/N): ${RESET}"
   read answer
   if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-    $ELEVATED_PRIVILEGE_CMD pacman -S git
+    $SUDO pacman -S git
   else
     printf "${RED}✗ Exiting...${RESET}\n"
     exit 1
@@ -74,7 +76,7 @@ if ! check_command stow; then
   printf "${YELLOW}Stow is not installed and is required, do you wish to install it? (y/N): ${RESET}"
   read answer
   if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-    $ELEVATED_PRIVILEGE_CMD pacman -S stow
+    $SUDO pacman -S stow
   else
     printf "${RED}✗ Exiting...${RESET}\n"
     exit 1
@@ -86,7 +88,7 @@ if [ "$IS_ARCH_BASED" = "true" ]; then
   printf "${CYAN}Do you wish to install all the packages needed to make the dotfiles work? (y/N): ${RESET}"
   read answer
   if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-    $ELEVATED_PRIVILEGE_CMD pacman -S foot zellij atuin bat neovim fd fzf lsd ripgrep zoxide starship grim slurp wl-clipboard libnotify brightnessctl playerctl hyprland hyprpicker hypridle hyprlock hyprsunset keepassxc
+    $SUDO pacman -S foot zellij atuin bat neovim fd fzf lsd ripgrep zoxide starship grim slurp wl-clipboard libnotify brightnessctl playerctl hyprland hyprpicker hypridle hyprlock hyprsunset keepassxc
   fi
 else
   printf "${YELLOW}Skipping packages install because the script is not running on an Arch based distro!${RESET}\n"
@@ -110,7 +112,19 @@ if [ ! -d "$CHOSEN_PATH" ]; then
     fi
 fi
 
-git clone https://github.com/Sawangg/dotfiles.git "$CHOSEN_PATH"
+# Clone the repository and handle existing directory
+if [ -d "$CHOSEN_PATH/dotfiles/.git" ]; then
+    printf "${GREEN}Existing git repository found. Updating...${RESET}\n"
+    cd "$CHOSEN_PATH/dotfiles"
+    git fetch origin
+    git pull origin master
+    cd -
+elif [ -d "$CHOSEN_PATH/dotfiles" ] && [ "$(ls -A "$CHOSEN_PATH/dotfiles" 2>/dev/null)" ]; then
+    printf "${RED}✗ Directory '$CHOSEN_PATH/dotfiles' exists but is not a git repository! Please remove it manually or choose a different path.${RESET}\n"
+    exit 1
+else
+    git clone https://github.com/Sawangg/dotfiles.git "$CHOSEN_PATH"
+fi
 
 # Configure apps to better match the environment using custom.conf
 custom_hypr="$CHOSEN_PATH/dotfiles/hypr/hyprland/custom.conf"
