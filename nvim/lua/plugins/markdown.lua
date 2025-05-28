81e2dda78318301b1ae1cb7fc5ae716449ce7fe4
Sawang <leo.mercier1@proton.me>
2025-05-28 11:53:43 +0200

    feat(nvim): removed obsidian plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,34 +1,4 @@
 return {
-  { -- Note taking plugin using obsidian
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      -- Path to your vaults
-      ui = { enable = false },
-      workspaces = {
-        {
-          name = "main",
-          path = "~/Documents/vaults/Main/",
-          -- This organization is specific to this vault
-          overrides = {
-            notes_subdir = "0-inbox",
-            new_notes_location = "notes_subdir",
-
-            disable_frontmatter = true,
-            templates = {
-              folder = "99-template/",
-            },
-            attachments = {
-              img_folder = "98-attachments/",
-            },
-          },
-        },
-      },
-    },
-  },
   { -- Better Markdown rendering
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown" },
