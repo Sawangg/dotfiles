6aa3dab81d626858d3791e824e9f48893a0e8d01
Sawangg <leo.mercier1@proton.me>
2025-06-26 18:38:20 +0200

    feat(nvim): added lint

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,30 @@
+return {
+  { -- Linting support
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+      {
+        "<leader>l",
+        function()
+          require("lint").try_lint()
+        end,
+        desc = "[L]int buffer",
+      },
+    },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        yaml = { "yamllint" },
+      }
+
+      -- Create autocmd to trigger linting on more events
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("nvim_lint", { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+}
