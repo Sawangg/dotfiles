6932e6934e912c596277fccaa4bc6006bebd0c40
Sawangg <leo.mercier1@proton.me>
2025-07-05 18:20:18 +0200

    fix(setup): added nodejs in lean.sh

--- lean.sh
+++ lean.sh
@@ -27,14 +27,14 @@ fi
 if command -v apt-get > /dev/null 2>&1; then
     printf "${CYAN}Using package manager: ${BOLD}apt-get${RESET}\n"
     $SUDO apt-get update
-    $SUDO apt-get install -y stow fzf ripgrep starship zoxide neovim
+    $SUDO apt-get install -y stow fzf ripgrep starship zoxide neovim nodejs
 elif command -v apk > /dev/null 2>&1; then
     printf "${CYAN}Using package manager: ${BOLD}apk${RESET}\n"
     $SUDO apk update
-    $SUDO apk add stow fzf ripgrep starship zoxide neovim
+    $SUDO apk add stow fzf ripgrep starship zoxide neovim nodejs
 elif command -v pacman > /dev/null 2>&1; then
     printf "${CYAN}Using package manager: ${BOLD}pacman${RESET}\n"
-    $SUDO pacman -Sy --noconfirm stow fzf ripgrep starship zoxide neovim
+    $SUDO pacman -Sy --noconfirm stow fzf ripgrep starship zoxide neovim nodejs
 else
     printf "${RED}Error: No supported package manager found${RESET}\n"
     exit 1
