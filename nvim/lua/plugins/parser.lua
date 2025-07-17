4ada2f2dac02717f4a851cfee6fc69c7f5c6fe7c
Sawangg <leo.mercier1@proton.me>
2025-07-17 22:50:15 +0200

    chore(nvim): renamed treesitter config file

---
+++ nvim/lua/plugins/parser.lua
@@ -0,0 +1,24 @@
+return {
+  { -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "bash", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  { -- Show code context
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+    opts = {
+      enable = true,
+    },
+  },
+}
