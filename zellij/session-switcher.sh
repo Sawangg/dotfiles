717e35922ec417acdfed5078ba55a44ad7e34f6b
Sawangg <leo.mercier1@proton.me>
2025-08-03 19:09:04 +0200

    feat(zellij): added complex session switcher

---
+++ zellij/session-switcher.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+DIRS="$HOME/Documents"
+SESSION_LIST=$(zellij list-sessions -n | awk '{print $1}')
+DIR_PATHS=$(find $DIRS -mindepth 1 -maxdepth 1 -type d)
+
+DIRS_WITHOUT_SESSION=""
+for DIR in $DIR_PATHS; do
+  TITLE=$(basename "$DIR")
+  if ! echo "$SESSION_LIST" | grep -qx "$TITLE"; then
+    DIRS_WITHOUT_SESSION="$DIRS_WITHOUT_SESSION$DIR
+"
+  fi
+done
+
+CHOICE=$( (printf "%s\n" $SESSION_LIST; printf "%s" "$DIRS_WITHOUT_SESSION") | fzf )
+
+if [ -z "$CHOICE" ]; then
+  exit 1
+fi
+
+if echo "$SESSION_LIST" | grep -qx "$CHOICE"; then
+  zellij pipe --plugin https://github.com/mostafaqanbaryan/zellij-switch/releases/download/0.2.1/zellij-switch.wasm -- "-s=${CHOICE} -l custom"
+else
+  SESSION_TITLE=$(basename "$CHOICE")
+  zellij --session "$SESSION_TITLE" --cwd "$CHOICE" --detach
+  zellij pipe --plugin https://github.com/mostafaqanbaryan/zellij-switch/releases/download/0.2.1/zellij-switch.wasm -- "-s=${SESSION_TITLE} -c=${CHOICE} -l custom"
+fi
