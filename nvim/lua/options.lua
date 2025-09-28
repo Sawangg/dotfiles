44acabe7140d3aad90d48c32708609df2aa628ae
Sawangg <leo.mercier1@proton.me>
2025-09-28 20:53:40 +0200

    feat(nvim): better options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,78 +1,55 @@
 -- See `:help vim.opt`
 -- For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.o.number = true
-vim.o.relativenumber = true
+-- Basic settings
+vim.o.number = true         -- Show line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.cursorline = true     -- Highlight current line where the cursor is
+vim.o.scrolloff = 10        -- Keep 10 lines above/below cursor
+vim.o.sidescrolloff = 8     -- Keep 8 columns left/right of cursor
+vim.o.textwidth = 120       -- Set the word wrap to 120 characters
 
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+-- Indentation settings
+vim.o.tabstop = 2        -- Tab width
+vim.o.shiftwidth = 2     -- Indent width
+vim.o.softtabstop = 2    -- Soft tab stop
+vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.smartindent = true -- Smart auto-indenting
+vim.o.autoindent = true  -- Copy indent from current line
+vim.o.breakindent = true -- Enable break indent
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end)
+-- Search settings
+vim.o.ignorecase = true    -- Case insensitive search
+vim.o.smartcase = true     -- Case-sensitive if uppercase in search
+vim.o.hlsearch = true      -- Highlight search results
+vim.o.incsearch = true     -- Show matches as you type
+vim.o.inccommand = "split" -- Preview of substitutions as you type
 
--- Enable break indent
-vim.o.breakindent = true
+-- Visual settings
+vim.o.termguicolors = true -- Enable 24-bit colors
+vim.o.signcolumn = "yes" -- Always show sign column
+vim.o.showmatch = true -- Highlight matching brackets
+vim.o.cmdheight = 1 -- Command line height
+vim.o.pumblend = 10 -- Pop-up menu transparency
+vim.o.winblend = 0 -- Floating window transparency
+vim.o.winborder = "rounded" -- Rounded borders for floating windows
+vim.o.showmode = false -- Don't show the mode (we're using a custom statusline)
+vim.o.list = true -- Show invisible characters
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- List of characters to show
 
--- Save undo history
-vim.o.undofile = true
+-- File handling
+vim.o.updatetime = 250 -- Faster completion
+vim.o.timeoutlen = 300 -- Key timeout duration
+vim.o.undofile = true  -- Save undo history
+vim.o.autoread = true  -- Auto reload files changed outside vim
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- Behavior settings
+vim.o.encoding = "UTF-8"                -- Set encoding
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
+vim.o.swapfile = false                  -- Disable swap files
+vim.o.errorbells = false                -- No error bells
+vim.o.confirm = true                    -- ask what do so about unsaved changes
 
--- Keep signcolumn on by default
-vim.o.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = "split"
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
--- 2 spaces indent
-vim.o.expandtab = true -- expand tab input with spaces characters
-vim.o.smartindent = true -- syntax aware indentations for newline inserts
-vim.o.tabstop = 2 -- num of space characters per tab
-vim.o.shiftwidth = 2 -- spaces per indentation level
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.o.hlsearch = true
-
--- Set the word wrap to 120 characters
-vim.o.textwidth = 120
-
--- Disable swap files
-vim.o.swapfile = false
-
--- Rounded borders for floating windows
-vim.o.winborder = "rounded"
+-- Split behavior
+vim.o.splitright = true -- Vertical splits go right
+vim.o.splitbelow = true -- Horizontal splits go below
