761e1f8bcf8b9c4063edff2b04778f820595990d
Sawangg <leo.mercier1@proton.me>
2025-09-28 21:21:05 +0200

    fix(nvim): moved keymaps to correct file

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -16,32 +16,6 @@ return {
           preview_border = "Normal",
         },
       })
-
-      local map = vim.keymap.set
-
-      map("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
-      map("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
-      map("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
-      map("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect FZF-Lua" })
-      map("n", "<leader>sw", fzf.grep_cword, { desc = "[S]earch current [W]ord" })
-      map("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
-      map("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics" })
-      map("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
-      map("n", "<leader>sm", fzf.marks, { desc = "[S]earch [M]arks" })
-      map("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      map("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
-
-      map("n", "<leader>/", function()
-        fzf.blines({ fzf_opts = { ["--layout"] = "reverse-list" }, winopts = { preview = { hidden = true } } })
-      end, { desc = "[/] Fuzzily search in current buffer" })
-
-      map("n", "<leader>s/", function()
-        fzf.live_grep({ grep_open_files = true, prompt = "Live Grep in Open Files" })
-      end, { desc = "[S]earch [/] in Open Files" })
-
-      map("n", "<leader>sn", function()
-        fzf.files({ cwd = vim.fn.stdpath("config"), hidden = true })
-      end, { desc = "[S]earch [N]eovim files" })
     end,
   },
 }
