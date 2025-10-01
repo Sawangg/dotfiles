dcd12280fec46ee0bf33752201ff1bb05cc0c410
Sawangg <leo.mercier1@proton.me>
2025-10-01 22:41:10 +0200

    fix(nvim): removed options that are enabled by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -15,42 +15,34 @@ vim.o.shiftwidth = 2     -- Indent width
 vim.o.softtabstop = 2    -- Soft tab stop
 vim.o.expandtab = true   -- Use spaces instead of tabs
 vim.o.smartindent = true -- Smart auto-indenting
-vim.o.autoindent = true  -- Copy indent from current line
 vim.o.breakindent = true -- Enable break indent
 
 -- Search settings
 vim.o.ignorecase = true    -- Case insensitive search
 vim.o.smartcase = true     -- Case-sensitive if uppercase in search
-vim.o.hlsearch = true      -- Highlight search results
-vim.o.incsearch = true     -- Show matches as you type
 vim.o.inccommand = "split" -- Preview of substitutions as you type
-
--- Visual settings
-vim.o.termguicolors = true -- Enable 24-bit colors
-vim.o.signcolumn = "yes" -- Always show sign column
-vim.o.cmdheight = 1 -- Command line height
-vim.o.pumblend = 10 -- Pop-up menu transparency
-vim.o.winblend = 0 -- Floating window transparency
-vim.o.winborder = "rounded" -- Rounded borders for floating windows
-vim.o.showmode = false -- Don't show the mode (we're using a custom statusline)
-vim.o.showtabline = 0 -- Hide the tab pages
-vim.o.list = true -- Show invisible characters
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- List of characters to show
-vim.opt.fillchars = { eob = " " } -- Disable end of buffer characters
 
 -- File handling
 vim.o.updatetime = 250 -- Faster completion
 vim.o.timeoutlen = 300 -- Key timeout duration
 vim.o.undofile = true  -- Save undo history
-vim.o.autoread = true  -- Auto reload files changed outside vim
 
 -- Behavior settings
-vim.o.encoding = "UTF-8"                -- Set encoding
-vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
 vim.o.swapfile = false                  -- Disable swap files
-vim.o.errorbells = false                -- No error bells
 vim.o.confirm = true                    -- Ask what do so about unsaved changes
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
 
 -- Split behavior
 vim.o.splitright = true -- Vertical splits go right
 vim.o.splitbelow = true -- Horizontal splits go below
+
+-- Visual settings
+vim.o.termguicolors = true -- Enable 24-bit colors
+vim.o.signcolumn = "yes" -- Always show sign column
+vim.o.pumblend = 10 -- Pop-up menu transparency
+vim.o.winborder = "rounded" -- Rounded borders for floating windows
+vim.o.showmode = false -- Don't show the mode (we're using a custom statusline)
+vim.o.showtabline = 0 -- Hide the tab pages
+vim.o.list = true -- Show invisible characters
+vim.opt.fillchars = { eob = " " } -- Disable end of buffer ~
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- List of characters to show
