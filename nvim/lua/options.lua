eed5a056bb783500e3ce66d7f5ed2e20b143eec3
Sawangg <leo.mercier1@proton.me>
2026-03-31 21:57:39 +0200

    fix(nvim): force unnamedplus to sync os & nvim clipboard

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,35 +2,37 @@
 -- For more options, you can see `:help option-list`
 
 -- Basic settings
-vim.o.number = true         -- Show line numbers
+vim.o.number = true -- Show line numbers
 vim.o.relativenumber = true -- Enable relative line numbers
-vim.o.cursorline = true     -- Highlight current line where the cursor is
-vim.o.scrolloff = 10        -- Keep 10 lines above/below cursor
-vim.o.sidescrolloff = 8     -- Keep 8 columns left/right of cursor
-vim.o.textwidth = 120       -- Set the word wrap to 120 characters
+vim.o.cursorline = true -- Highlight current line where the cursor is
+vim.o.scrolloff = 10 -- Keep 10 lines above/below cursor
+vim.o.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+vim.o.textwidth = 120 -- Set the word wrap to 120 characters
 
 -- Indentation settings
-vim.o.tabstop = 2        -- Tab width
-vim.o.shiftwidth = 2     -- Indent width
-vim.o.softtabstop = 2    -- Soft tab stop
-vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.tabstop = 2 -- Tab width
+vim.o.shiftwidth = 2 -- Indent width
+vim.o.softtabstop = 2 -- Soft tab stop
+vim.o.expandtab = true -- Use spaces instead of tabs
 vim.o.smartindent = true -- Smart auto-indenting
 vim.o.breakindent = true -- Enable break indent
 
 -- Search settings
-vim.o.ignorecase = true    -- Case insensitive search
-vim.o.smartcase = true     -- Case-sensitive if uppercase in search
+vim.o.ignorecase = true -- Case insensitive search
+vim.o.smartcase = true -- Case-sensitive if uppercase in search
 vim.o.inccommand = "split" -- Preview of substitutions as you type
 
 -- File handling
 vim.o.updatetime = 250 -- Faster completion
 vim.o.timeoutlen = 300 -- Key timeout duration
-vim.o.undofile = true  -- Save undo history
+vim.o.undofile = true -- Save undo history
 
 -- Behavior settings
-vim.o.swapfile = false                  -- Disable swap files
-vim.o.confirm = true                    -- Ask what do so about unsaved changes
-vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
+vim.o.swapfile = false -- Disable swap files
+vim.o.confirm = true -- Ask what do so about unsaved changes
+vim.schedule(function() -- Use system clipboard
+  vim.o.clipboard = "unnamedplus"
+end)
 
 -- Split behavior
 vim.o.splitright = true -- Vertical splits go right
